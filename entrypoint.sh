9bb437f902c659a7f456e096f46cf6a6c1d4b120
mcasals <mcasals@fie.undef.edu.ar>
2025-03-20 11:15:20 -0300

    prueba franco

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,9 +11,9 @@ python manage.py migrate
 echo "Creando superusuario si no existe..."
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(is_superuser=True).exists() or User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')" | python manage.py shell
 
-echo "Recolectando archivos estáticos..."
+echo "Recolectando archivos estáticos..."#se hacen los estaticos para sacarlo a produccion<-preguntar
 python manage.py collectstatic --noinput
 
-echo "Iniciando servidor de Django..."
+echo "Iniciando servidor de Django..."#se esta ejecutando dos veces, una en el dockerfile y otra aca...
 python manage.py runserver 0.0.0.0:8000
 
